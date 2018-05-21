9625d1e2727a67b98013c974b66de2fbb9a62ac1
TonyPuricelli <tpuricelli@gmail.com>
2018-05-21 11:57:19 -0500

    initial commit -- adds folder structure, basic logic

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var burgers = require("../models/burger.js");
+
+router.get("/", function(req, res){
+	burger.all(function(data){
+		var burgerList = {burgers: data};
+
+		console.log(burgerList);
+
+		res.render("index", burgerList);
+	});
+});
+
+router.post("/burgers", function(req, res){
+	burger.insert(['burger_name'], [req.body.b_name], function(data){
+		res.redirect('/')
+	});
+});
+
+router.put("/burgers/:id", function(req, res){
+	var condition = "id = " + req.params.id;
+
+	console.log("condition ", condition);
+
+	burger.update({
+        devoured: req.body.devoured
+    }, condition, function(data) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then ID must not exist
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+	});
+});
+
+router.delete("/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+});
+
+// Export routes for server.js
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS  `burgers_db`;
+
+USE `burgers_db`;
+
+/* Create a burgers table */
+CREATE TABLE `burgers` (
+	`id` Int(11) AUTO_INCREMENT NOT NULL,
+	`burger_name` VARCHAR(255) NOT NULL,
+	`devoured` BOOLEAN DEFAULT false,
+	`date` TIMESTAMP,
+	PRIMARY KEY `id`
+);
